d6fa21e2509950b2f4c74e73481c934a2ad7aa78
Peter Gramenides <Peter.Gramenides@ibm.com>
2020-07-29 10:46:24 -0400

    added appsody and buildah
    - added appsody and buildah
    - added dependencies such as runc and google protocol buffers
    - fixed install_utilities so kubectl installs properly
    - updated console output for new dependencies

--- pipelines/docker/pipelines-utils/build_image.sh
+++ pipelines/docker/pipelines-utils/build_image.sh
@@ -1,11 +1,11 @@
-# install dependencies on infastructure node
-yum -y install sudo
-sudo yum -y install buildah
-sudo yum -y install podman
-sudo yum -y install skopeo
-sudo yum -y install runc
-sudo yum -y install slirp4netns
-sudo yum module install -y container-tools
+# install dependencies on infastructure node (run on first install)
+#yum -y install sudo
+#sudo yum -y install buildah
+#sudo yum -y install podman
+#sudo yum -y install skopeo
+#sudo yum -y install runc
+#sudo yum -y install slirp4netns
+#sudo yum module install -y container-tools
 
 # build redhat ubi from dockerfile
 buildah bud -t ibmpipelines .
