8c3305fc0ed267a149a77a896cb29c9a37581396
Michael Cheng <mcheng@us.ibm.com>
2021-03-16 10:47:41 -0500

    update copyright

--- pipelines/docker/pipelines-utils/build_utils.sh
+++ pipelines/docker/pipelines-utils/build_utils.sh
@@ -1,3 +1,16 @@
+# (C) Copyright IBM Corporation 2019, 2021
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #!/bin/sh
 # This script will build 'pipelines-utils' docker image with given image tagname and based on the dockerhub credentials given to it. The script will
 # push the image to the approprite registry. Also along with the image tagname the image will be marked as 'latest' tag and pushed to the dockerhub as well.
--- pipelines/docker/pipelines-utils/scripts/ca_certs_setup.sh
+++ pipelines/docker/pipelines-utils/scripts/ca_certs_setup.sh
@@ -1,3 +1,16 @@
+# (C) Copyright IBM Corporation 2019, 2021
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #!/bin/sh
 # This script will be used in later tasks to fetch the trusted cs certificates configured 
 # in a configmap which is set in the 'image.config.openshift.io/cluster' resource by the user, and we will copy the certificate values and generate 'ca.crt' as '/etc/docker/certs.d/<hostname>/ca.crt' for each certificate on the container.
